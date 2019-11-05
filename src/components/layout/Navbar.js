18ae4a9f5f3f4ac43669024016dbc15a679bb0be
King Elisha <ek.chibueze@gmail.com>
2019-11-05 16:44:07 +0100

    landing page

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.scss';
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <nav>
+        <h1>
+          Logo
+        </h1>
+      </nav>
+    </div>
+  )
+}
--- src/components/routes/Landing.js
+++ src/components/routes/Landing.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Navbar from '../layout/Navbar';
 
 export default class Landing extends Component {
   render() {
     return (
       <div>
-        <h1>Landing</h1>
+        <Navbar />
       </div>
     )
   }
