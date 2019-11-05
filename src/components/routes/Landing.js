a28c4ee3b97de9949844cbb8d3e51c5988a9d6e4
King Elisha <ek.chibueze@gmail.com>
2019-11-05 18:16:32 +0100

    home markup

--- src/components/routes/Landing.js
+++ src/components/routes/Landing.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
+import './Landing.scss';
 import Navbar from '../layout/Navbar';
+import art from '../../assets/images/art.svg';
 
 export default class Landing extends Component {
   render() {
     return (
       <div id="landing">
-        <Navbar />
-
         <div className="home">
-          <div className="left">
-            <h1>
-              VP
-            </h1>
-          </div>
+          <Navbar />
 
-          <div className="right">
-            Art
+          <div className="content">
+            <div className="left">
+              <h1>
+                We build stunning websites, web apps and mobile apps
+              </h1>
+
+              <p>
+                We a dedicated to giving your brand by building a unique and compelling digital presence
+              </p>
+
+              <a href="#contact">Start a Project</a>
+            </div>
+
+            <div className="right">
+              <img src={art} alt="Web/App Design and Development" />
+            </div>
           </div>
         </div>
       </div>
