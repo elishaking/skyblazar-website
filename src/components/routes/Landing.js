35151c003a73687e4127bab1ff6dd7a42f7eb97c
King Elisha <ek.chibueze@gmail.com>
2019-11-05 18:03:12 +0100

    home

--- src/components/routes/Landing.js
+++ src/components/routes/Landing.js
@@ -4,8 +4,20 @@ import Navbar from '../layout/Navbar';
 export default class Landing extends Component {
   render() {
     return (
-      <div>
+      <div id="landing">
         <Navbar />
+
+        <div className="home">
+          <div className="left">
+            <h1>
+              VP
+            </h1>
+          </div>
+
+          <div className="right">
+            Art
+          </div>
+        </div>
       </div>
     )
   }
