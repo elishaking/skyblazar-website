07fb524b03fad0a1e59560192582a011d79002e5
King Elisha <ek.chibueze@gmail.com>
2019-11-05 16:47:53 +0100

    logo

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Navbar.scss';
+import Logo from '../Logo';
 
 export default function Navbar() {
   return (
     <div className="navbar">
       <nav>
         <h1>
-          Logo
+          <Logo />
+          <span>Skyblazar</span>
         </h1>
       </nav>
     </div>
