088ee710aebb00cf7ae8b5deb0dbe1ef097a5f3d
King Elisha <ek.chibueze@gmail.com>
2019-11-06 11:24:13 +0100

    close icon

--- src/components/ImageModal.js
+++ src/components/ImageModal.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './ImageModal.scss';
+import Close from './icons/Close';
 
 export default function ImageModal({ imgSrc, imgCaption, imgDescription, close }) {
   return (
     <div id="image-modal" className="image-modal" onClick={close}>
       <span className="close" onClick={close} >
-        <svg xmlns="http://www.w3.org/2000/svg" width="23.024" height="23.024" viewBox="0 0 23.024 23.024">
-          <g id="Group_43" data-name="Group 43" transform="translate(-219.086 -1192.086)">
-            <line id="Line_4" data-name="Line 4" x2="20.195" y2="20.195" transform="translate(220.5 1193.5)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="2" />
-            <line id="Line_5" data-name="Line 5" x1="20.195" y2="20.195" transform="translate(220.5 1193.5)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="2" />
-          </g>
-        </svg>
+        <Close />
       </span>
 
       <img onClick={(e) => { e.stopPropagation() }} className="modal-content" src={imgSrc} alt="Project" />
---
+++ src/components/icons/Close.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Close() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="23.024" height="23.024" viewBox="0 0 23.024 23.024">
+      <g id="Group_43" data-name="Group 43" transform="translate(-219.086 -1192.086)">
+        <line id="Line_4" data-name="Line 4" x2="20.195" y2="20.195" transform="translate(220.5 1193.5)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="2" />
+        <line id="Line_5" data-name="Line 5" x1="20.195" y2="20.195" transform="translate(220.5 1193.5)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="2" />
+      </g>
+    </svg>
+  )
+}
